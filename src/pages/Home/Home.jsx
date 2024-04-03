59728e1d5c99060797ccaf0344e914af5aa615b0
Fran <franrocher95@gmail.com>
2024-04-03 21:04:41 +0200

    feat:creating Login page and adding login user function

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+export const Home = () => {
+  //Instancia de Redux en modo lectura para home
+
+
+
+
+
+  return <div className="d-flex justify-content-center align-items-center home-design">soy home</div>;
+};
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,67 @@
+import "./Login.css";
+import { useState } from "react";
+
+import { loginService } from "../../services/apiCalls";
+import { useNavigate } from "react-router-dom";
+import { decodeToken } from "react-jwt";
+//Redux
+
+import { login } from "../../app/slices/userSlice";
+import { useDispatch } from "react-redux";
+import { CustomInput } from "../../components/CustomInput/CustomInput";
+
+export const Login = () => {
+  const navigate = useNavigate();
+
+  //Instancia de Redux para escritura
+  const dispatch = useDispatch();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputHandler = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const loginMe = async () => {
+    const fetched = await loginService(user);
+
+    if (fetched.token) {
+      const decodificado = decodeToken(fetched.token);
+
+      const passport = {
+        token: fetched.token,
+        user: decodificado,
+      };
+
+      dispatch(login({ credentials: passport }));
+
+      setTimeout(()=> {
+        navigate("/")
+      }, 500)
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center flex-column align-items-center login-design">
+      <CustomInput
+        type="email"
+        name="email"
+        value={user.email || ""}
+        changeEmit={inputHandler}
+      />
+      <CustomInput
+        type="password"
+        name="password"
+        value={user.password || ""}
+        changeEmit={inputHandler}
+      />
+      <button onClick={loginMe}>Login</button>
+    </div>
+  );
+};
