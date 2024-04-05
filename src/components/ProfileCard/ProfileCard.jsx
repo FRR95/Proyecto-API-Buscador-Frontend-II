dd70c107e025322607b9cc04ee76bc3c75b4206e
Fran <franrocher95@gmail.com>
2024-04-05 19:22:28 +0200

    update:updating profile card component

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,37 +1,35 @@
+import { CustomButton } from "../CustomButton/CustomButton"
 import "./ProfileCard.css"
 
-export const ProfileCard = ({ buttonEditDesign, buttonEditTitle, buttonDeleteDesign, buttonDeleteTitle, username, email, idUser, password }) => {
+export const ProfileCard = ({ buttonEditDesign, buttonEditTitle, buttonDeleteDesign, buttonDeleteTitle, username, email,buttonSectionDesign,buttonEditSection,buttonDeleteSection,emitDeleteButton,emitEditButton }) => {
 
     return (
         <div className="d-flex row justify-content-center align-items-center profileCardDesign">
 
-            <div className="d-flex row justify-content-center align-items-center" >
-                {idUser}
-            </div>
-            <div className="d-flex row justify-content-center align-items-center" >
-                {password}
-            </div>
-
-
+          
+         
             <div className="d-flex row justify-content-center align-items-center" >
                 {username}
             </div>
             <div className="d-flex row justify-content-center align-items-center" >
                 {email}
             </div>
-            <div className="d-flex row justify-content-center align-items-center" >
-                <div className="d-flex col justify-content-center align-items-center">
-                    <button className={buttonEditDesign}>
+            <div className={buttonSectionDesign} >
+                <div className={buttonEditSection}>
+                    <button onClick={emitEditButton} className={buttonEditDesign}>
                         {buttonEditTitle}
                     </button>
 
                 </div>
-                <div className="d-flex col justify-content-center align-items-center">
-                    <button className={buttonDeleteDesign}>
+                <div className={buttonDeleteSection}>
+                    <button onClick={emitDeleteButton} className={buttonDeleteDesign}>
                         {buttonDeleteTitle}
                     </button>
                 </div>
             </div>
+
+
+           
         </div>
     )
 
