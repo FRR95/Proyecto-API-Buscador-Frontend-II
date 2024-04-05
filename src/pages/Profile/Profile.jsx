dead6533cb4473c6d3db00fe78c09d90a5e8e3a5
Fran <franrocher95@gmail.com>
2024-04-05 19:30:48 +0200

    feat:adding post form and user form to update user info and adding create post and update user info function in profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,45 +4,101 @@ import { useSelector } from "react-redux";
 import { userData } from "../../app/slices/userSlice";
 import { useEffect, useState } from "react";
 import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
-import { GetProfile } from "../../services/apiCalls";
+import { CreatePost, GetProfile, UpdateProfile } from "../../services/apiCalls";
 import { CustomInput } from "../../components/CustomInput/CustomInput";
+import { CustomButton } from "../../components/CustomButton/CustomButton";
 
 export const Profile = () => {
   const navigate = useNavigate();
 
   const [loadedData, setLoadedData] = useState(false);
+  const [editboolean, setEditBoolean] = useState(false);
+
+
+
 
   const [user, setUser] = useState({
     name: "",
     email: "",
   });
 
+  const inputHandler = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+
+  const [postCredentials, setPostCredentials] = useState({
+    title: "",
+    description: "",
+  });
+
+  const postHandler = (e) => {
+    setPostCredentials((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   const rdxUser = useSelector(userData)
 
   useEffect(() => {
-    console.log(`fdfssfasd ${rdxUser.credentials.token}`);
     if (!rdxUser.credentials.token) {
       navigate("/")
     }
   }, [rdxUser])
 
+  const getUserProfile = async () => {
+    try {
+      const fetched = await GetProfile(rdxUser.credentials.token);
+      setLoadedData(true);
+     
+
+      setUser({
+        name: fetched.data.name,
+        email: fetched.data.email,
+      });
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const changeboolean = () => {
+    setEditBoolean(true)
+  }
+
+  const editProfile = async () => {
+    const fetched = await UpdateProfile(rdxUser.credentials.token, user)
+    if (!fetched.success) {
+      console.log(fetched.message)
+    }
+
+    console.log(fetched)
+
+    setTimeout(() => {
+      getUserProfile()
+      setEditBoolean(false)
+    }, 500);
+
+  }
+
+  const createPost = async () => {
+    const fetched = await CreatePost(rdxUser.credentials.token, postCredentials)
+    if (!fetched.success) {
+      console.log(fetched.message)
+    }
+    console.log(fetched.message)
+
+    setPostCredentials({
+      title: "",
+      email: "",
+    });
+  }
+
   useEffect(() => {
-
-    const getUserProfile = async () => {
-      try {
-        const fetched = await GetProfile(rdxUser.credentials.token);
-        setLoadedData(true);
-        console.log(fetched)
-
-        setUser({
-          name: fetched.data.name,
-          email: fetched.data.email,
-        });
-
-      } catch (error) {
-        console.log(error);
-      }
-    };
 
     if (!loadedData) {
       getUserProfile();
@@ -54,28 +110,76 @@ export const Profile = () => {
 
 
   return (
-    <div className="d-flex justify-content-center row  align-items-center profileDesign">
-      <div className="d-flex justify-content-center row  align-items-center">
-        <ProfileCard
-          idUser={"idUser"}
-          key={"key"}
-          password={"password"}
-          username={user.name}
-          email={user.email}
-          buttonEditTitle={"buttonEditTitle"}
-          buttonDeleteTitle={"buttonDeleteTitle"}
-          buttonEditDesign={"buttonEditDesign"}
-          buttonDeleteDesign={"buttonEditDesign"}
+    <>
+      <div className="d-flex justify-content-center row  align-items-center profileDesign">
 
-        />
+        {editboolean
+          ? (
+            <div className="d-flex justify-content-center flex-column  align-items-center">
+              <CustomInput
+                type="text"
+                name="name"
+                value={user.name || ""}
+                changeEmit={inputHandler}
+              />
+              <CustomButton
+                onClick={editProfile}
+                design={""}
+                title={`Editar ${user.name}`} />
+            </div>)
+          : (
+
+            <div className="d-flex justify-content-center row  align-items-center">
+              <ProfileCard
+                idUser={"idUser"}
+                key={"key"}
+                username={user.name}
+                email={user.email}
+                buttonSectionDesign={"d-flex row justify-content-center align-items-center"}
+
+                buttonDeleteSection={"d-none col justify-content-center align-items-center"}
+                buttonEditSection={"d-flex col justify-content-center align-items-center"}
+                buttonEditTitle={`Editar ${user.name}`}
+                buttonDeleteTitle={`Editar ${user.name}`}
+                buttonEditDesign={"buttonEditDesign"}
+                emitEditButton={changeboolean}
+
+
+              />
+            </div>)
+
+        }
+
+        <div className="d-flex mt-5 justify-content-center row  align-items-center">
+          <div className="d-flex justify-content-center row  align-items-center">
+            <p>Titulo</p>
+            <CustomInput
+              type="text"
+              name="title"
+              design={"input-design"}
+              value={postCredentials.title || ""}
+              changeEmit={postHandler}
+            />
+          </div>
+          <div className="d-flex justify-content-center row  align-items-center">
+            <p>Descripción</p>
+            <CustomInput
+              type="text"
+              name="description"
+              design={"input-design-big"}
+              value={postCredentials.description || ""}
+              changeEmit={postHandler}
+            />
+          </div>
+          <div className="d-flex justify-content-center flex-column  align-items-center">
+            <CustomButton
+              onClick={createPost}
+              design={""}
+              title={`Post`}
+            />
+          </div>
+        </div>
       </div>
-      <div className="d-flex justify-content-center row  align-items-center">
-        <CustomInput
-          design={"input-design-big"}
 
-        />
-      </div>
-
-    </div>
-  )
+    </>)
 }
