59a2d2b78383cc2f3ead8d819ee7df93808daa2a
Fran <franrocher95@gmail.com>
2024-04-06 18:51:03 +0200

    feat:adding timeline posts in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,81 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
+import { useSelector } from "react-redux";
+import { userData } from "../../app/slices/userSlice";
+import { getTimeline } from "../../services/apiCalls";
+import { PostCard } from "../../components/PostCard/PostCard";
 export const Home = () => {
   //Instancia de Redux en modo lectura para home
 
+  const rdxUser = useSelector(userData)
+
+  const [followingPost, setFollowingPost] = useState([])
+
+  useEffect(() => {
+   
+  }, [rdxUser])
+
+  const BringTimelinePosts = async () => {
+    try {
+
+
+      const fetched = await getTimeline(rdxUser.credentials.token)
+      setFollowingPost(fetched.data)
+      console.log(fetched)
+   
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() => {
+    if(rdxUser?.credentials.token){
+    if (followingPost.length === 0) {
+      BringTimelinePosts()
+    }
+  }
+
+  }, [followingPost])
 
 
 
 
-  return <div className="d-flex justify-content-center align-items-center home-design">soy home</div>;
+
+
+
+  return (
+    <>
+      {!rdxUser?.credentials.token
+        ? (<div className="d-flex justify-content-center align-items-center home-design">No estas logueado</div>)
+        : (<div className="d-flex justify-content-center align-items-center home-design">
+          {followingPost.length > 0
+          ?(<div>
+            {followingPost.map(
+              post =>{
+                return(
+                  <>
+                  <PostCard
+                  
+                  title={post.title}
+                  description={post.description}
+                  buttonsSection={"d-none"}
+               
+                  />
+                  </>
+                )
+              }
+            )}
+          </div>)
+          :("No tienes posts de seguidos")
+          
+          }
+        </div>)
+
+      }
+
+    </>
+
+  )
 };
