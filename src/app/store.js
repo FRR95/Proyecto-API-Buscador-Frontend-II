d6ab92e05f3486e3ec7a49ca1127ce85bbd2cfc6
Fran <franrocher95@gmail.com>
2024-04-08 19:17:23 +0200

    feat:adding profileDetailSlice in the store file

--- src/app/store.js
+++ src/app/store.js
@@ -7,11 +7,13 @@ import storage from "redux-persist/lib/storage";
 
 import userSlice from "./slices/userSlice";
 import postDetailSlice from "./slices/postDetailSlice";
+import profileDetailSlice from "./slices/profileDetailSlice";
 
 
 const reducers = combineReducers({
   user: userSlice,
-  postDetail:postDetailSlice
+  postDetail:postDetailSlice,
+  profileDetail:profileDetailSlice
 });
 
 const persistConfig = {
