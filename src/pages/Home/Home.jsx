9a7dbc903e3dbfcf2a6db60b6a4f53bf2ee81570
Fran <franrocher95@gmail.com>
2024-04-10 21:39:33 +0200

    feat:adding home page in case of not logged user

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import { LikeDislikePost, getTimeline } from "../../services/apiCalls";
 import { PostCard } from "../../components/PostCard/PostCard";
 
 import { ToastContainer, toast } from 'react-toastify';
+import { CustomLink } from "../../components/CustomLink/CustomLink";
 
 export const Home = () => {
   //Instancia de Redux en modo lectura para home
@@ -20,8 +21,8 @@ export const Home = () => {
 
       const fetched = await getTimeline(rdxUser.credentials.token)
       setFollowingPost(fetched.data)
-   
-   
+
+
 
     } catch (error) {
       toast.error(error)
@@ -30,35 +31,35 @@ export const Home = () => {
   }
 
   useEffect(() => {
-   
+
   }, [rdxUser])
 
   useEffect(() => {
-    if(rdxUser?.credentials.token){
-    if (followingPost.length === 0) {
-      BringTimelinePosts()
+    if (rdxUser?.credentials.token) {
+      if (followingPost.length === 0) {
+        BringTimelinePosts()
+      }
     }
-  }
 
   }, [followingPost])
 
 
 
-  const likeUnlikePost =async(postId)=>{
-    
+  const likeUnlikePost = async (postId) => {
+
     try {
-      const fetched =await LikeDislikePost(postId,rdxUser.credentials.token)
-    
-      if(!fetched.success){
+      const fetched = await LikeDislikePost(postId, rdxUser.credentials.token)
+
+      if (!fetched.success) {
         toast.error(`${fetched.message}`)
       }
       toast(`💗 ${fetched.message}`)
       BringTimelinePosts()
-    
+
     } catch (error) {
       toast.error(error)
     }
-      }
+  }
 
 
 
@@ -69,39 +70,53 @@ export const Home = () => {
   return (
     <>
       {!rdxUser?.credentials.token
-        ? (<div className="  d-flex  justify-content-center align-items-center home-design">No estas logueado</div>)
+        ? (<div className="  d-flex row   justify-content-center align-items-center home-design">
+
+          <div className="  d-flex col-xl  col-lg  row-sm row-xs   justify-content-center align-items-center ">
+            <div className="  d-flex row p-5  justify-content-center align-items-center rightBlockDesign ">
+             <img src="https://www.investopedia.com/thmb/tFHjCFL9uLlgj5_yQ6xt6WzH7iQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/social-media-final-8f48359ac9e7486eaf40932f4a9e2597.png" height="320em" width={"380em"} alt="SocialMedia" />
+            </div>
+          </div>
+          <div className="  d-flex col-xl  col-lg  row-sm row-xs justify-content-center align-items-center ">
+            <div className="  d-flex row p-5  justify-content-center align-items-center rightBlockDesign ">
+              <h3>Bienvenido a SocialMedia</h3>
+              <p> <CustomLink path={"/register"} title={"Regístrate"} />  o <CustomLink path={"/login"} title={"Inicia sesión"} /> para acceder a nuestra red social</p>
+            </div>
+          </div>
+
+        </div>)
         : (<div className=" d-flex  justify-content-center align-items-center home-design">
           {followingPost.length > 0
-          ?(<div>
-            {followingPost.map(
-              post =>{
-                return(
-                  <>
-                  <PostCard
-                  
-                  title={post.title}
-                  description={post.description}
-                  buttonsSection={"d-none"}
-                  buttonLikeSection={"d-flex justify-content-end"}
-                  emitLikeButton={() => likeUnlikePost(post._id)}
-                  numberOflikes={post.numberOfLikes.length}
-                  // userName={post.userId.email}
-               
-                  />
-                  </>
-                )
-              }
-            )}
-          </div>)
-          :("No tienes posts de seguidos")
-          
+            ? (<div>
+              {followingPost.map(
+                post => {
+                  return (
+                    <>
+                      <PostCard
+
+                        title={post.title}
+                        description={post.description}
+                        buttonsSection={"d-none"}
+                        buttonLikeSection={"d-flex justify-content-end"}
+                        emitLikeButton={() => likeUnlikePost(post._id)}
+                        numberOflikes={post.numberOfLikes.length}
+                      // userName={post.userId.email}
+
+                      />
+                    </>
+                  )
+                }
+              )}
+            </div>)
+            : ("No tienes posts de seguidos")
+
           }
         </div>)
 
-        
+
 
       }
-     <ToastContainer
+      <ToastContainer
         position="top-center"
         autoClose={5000}
         hideProgressBar={false}
